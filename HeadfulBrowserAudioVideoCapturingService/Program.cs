3078b6fb26d5430f3203456c8ecc1c7327af2747
Sergei Iaskov <sergeiiaskov@microsoft.com>
2022-04-20 17:28:43 +0300

    Add simple PuppeteerSharp usage.

--- HeadfulBrowserAudioVideoCapturingService/Program.cs
+++ HeadfulBrowserAudioVideoCapturingService/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using PuppeteerSharp;
+
+Console.WriteLine("Started...");
+using var browserFetcher = new BrowserFetcher();
+await browserFetcher.DownloadAsync();
+await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true });
+await using var page = await browser.NewPageAsync();
+await page.GoToAsync("https://www.google.com");
+await page.ScreenshotAsync("screenshot.png");
